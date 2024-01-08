871aa53c4551d801a37e27b212cf700def4eeba7
chatchanunn nn <chatchanunn@gmail.com>
2024-01-08 08:42:54 +0000

    ongitpod

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <div>
+        Profile Vue
+        <div>
+            First Name <input type="text">
+        </div>
+        <div>
+            Last Name <input type="text">
+        </div>        
+    </div>
+    <div>
+        <RouterLink :to="{ name: 'home'}"> 
+            <button> back to home </button>
+        </RouterLink>
+    </div>
+</template>
